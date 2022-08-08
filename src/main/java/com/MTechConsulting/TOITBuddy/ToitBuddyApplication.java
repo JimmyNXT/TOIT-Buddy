73f4282cc87c325b4fa235d58fd17526f9e93465
Dean Minnie <dminnie33@gmail.com>
2022-08-08 17:53:05 +0200

    Added missing files

---
+++ src/main/java/com/MTechConsulting/TOITBuddy/ToitBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.MTechConsulting.TOITBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToitBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToitBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MTechConsulting/TOITBuddy/ToitBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MTechConsulting.TOITBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToitBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
